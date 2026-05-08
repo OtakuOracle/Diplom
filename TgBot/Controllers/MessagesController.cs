be93b494a3c2b628094c2f08f5e0b95ddf17d938
Nastya <nastya.vakhitova700@mail.ru>
2026-05-08 13:09:37 +0300

    dsдде

--- TgBot/Controllers/MessagesController.cs
+++ TgBot/Controllers/MessagesController.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Telegram.Bot;
 using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.ReplyMarkups; 
+using Newtonsoft.Json;
 
 namespace TgBot.Controllers
 {
@@ -20,65 +19,33 @@ namespace TgBot.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Post([FromBody] Update update)
+        public async Task<IActionResult> Post([FromBody] object rawUpdate)
         {
-            if (update == null) return Ok();
-
-            if (update.Type == UpdateType.Message && update.Message?.Text != null)
-            {
-                var message = update.Message;
-                var chatId = message.Chat.Id;
-
-                if (message.Text.StartsWith("/start"))
-                {
-                    await SendWelcomeMenu(chatId);
-                }
-            }
-
-            if (update.Type == UpdateType.CallbackQuery && update.CallbackQuery != null)
-            {
-                var callbackQuery = update.CallbackQuery;
-                _logger.LogInformation($"Нажата кнопка: {callbackQuery.Data}");
-
-                if (callbackQuery.Data == "help")
-                {
-                    await _botClient.SendMessage(callbackQuery.Message.Chat.Id, "Тут будет инструкция...");
-                }
-
-                return Ok();
-            }
-
-            return Ok();
-        }
-
-        private async Task SendWelcomeMenu(long chatId)
-        {
-            var inlineKeyboard = new InlineKeyboardMarkup(new[]
-            {
-                // Первый ряд кнопок
-                new []
-                {
-                    InlineKeyboardButton.WithCallbackData("❓ Что можно сделать", "help"),
-                    InlineKeyboardButton.WithCallbackData("📚 Обзор", "review"),
-                },
-
-            });
-
-            string welcomeText = "👋 *Добро пожаловать!*\n\nВыберите действие ниже:";
+            _logger.LogInformation(">>> ПРИШЕЛ ЗАПРОС ОТ ТЕЛЕГРАМ!");
 
             try
             {
-                await _botClient.SendMessage(
-                    chatId: chatId,
-                    text: welcomeText,
-                    parseMode: ParseMode.Markdown,
-                    replyMarkup: inlineKeyboard 
-                );
+                var json = rawUpdate.ToString();
+                _logger.LogInformation($"JSON данных: {json}");
+
+                var update = JsonConvert.DeserializeObject<Update>(json);
+
+                if (update?.Message != null)
+                {
+                    _logger.LogInformation($"Текст сообщения: {update.Message.Text}");
+
+                    if (update.Message.Text == "/start")
+                    {
+                        await _botClient.SendMessage(update.Message.Chat.Id, "Бот видит вас!");
+                    }
+                }
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Ошибка при отправке меню");
+                _logger.LogError($"ОШИБКА ОБРАБОТКИ: {ex.Message}");
             }
+
+            return Ok();
         }
     }
 }
